1f2c580a96737595e57d3184db20135f97dfed0d
shenxuxiang <shen_xuxiang@163.com>
2023-05-17 13:39:28 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# npm run dev
+启动开发服务器
+
+# npm run build
+构建打包
---
+++ src/global.d.ts
@@ -0,0 +1,71 @@
+type CSSModuleClasses = { readonly [ key: string ]: string }
+
+declare module '*.module.css' {
+  const classes: CSSModuleClasses;
+  export default classes;
+}
+
+declare module '*.module.less' {
+  const classes: CSSModuleClasses;
+  export default classes;
+}
+
+declare module '*.css' {
+  const css: string;
+  export default css;
+}
+
+declare module '*.less' {
+  const css: string;
+  export default css;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: string;
+  export default src;
+}
+
+declare module '.webp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.bmp' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.mp4' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.mp3' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.ogg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webm' {
+  const src: string;
+  export default src;
+}
